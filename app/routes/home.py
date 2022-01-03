1ad8651ffc6e315a7a6e18d7741dc9e9341bef92
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2022-01-02 18:58:28 -0600

    added session functionality

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect
 from app.models import Post
 from app.db import get_db
 
@@ -19,7 +19,8 @@ def index():
 
   return render_template(
   'homepage.html',
-  posts=posts
+  posts=posts,
+  loggedIn=session.get('loggedIn')
 )
 # The get_db() function returns a session connection that's tied to this route's context. 
 # We then use the query() method on the connection object to query the Post model for all posts in descending order, and we save the results in the posts variable.  
@@ -37,9 +38,10 @@ def single(id):
 
   # render single post template
   return render_template(
-    'single-post.html',
-    post=post
-  )
+  'single-post.html',
+  post=post,
+  loggedIn=session.get('loggedIn')
+)
 # This time, we use the filter() method on the connection object to specify the SQL WHERE clause, and we end by using the one() method instead of all(). 
 # We then pass the single post object to the single-post.html template. 
 # Once the template is rendered and the response sent, the context for this route terminates, and the teardown function closes the database connection.  
